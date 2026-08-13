b54ee0b8c7836a9617c47a0b8eb2d63cdb8ac485
Krishnarani2011 <koletena2011@gmail.com>
2026-08-13 11:19:59 +0530

    Add ConstructorOverloading class with Student example

---
+++ Ex3/ConstructorOverloading.java
@@ -0,0 +1,33 @@
+class Student {
+
+    int id;
+    String name;
+
+    // Default constructor
+    Student() {
+        id = 0;
+        name = "Unknown";
+    }
+
+    // Parameterized constructor
+    Student(int i, String n) {
+        id = i;
+        name = n;
+    }
+
+    void display() {
+        System.out.println("ID: " + id + " Name: " + name);
+    }
+}
+
+public class ConstructorOverloading {
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+        Student s2 = new Student(102, "Bhargavi");
+
+        s1.display();
+        s2.display();
+    }
+}
+
